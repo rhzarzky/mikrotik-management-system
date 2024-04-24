e2d8c67c6c83f21bab45c8bf212b9bb85ac94c45
Rheza Rizky <rhezarizky11@gmail.com>
2024-04-24 08:07:52 +0700

    cobain postgresql laragon, belom bisa

--- GMEDIA/database/seeders/DatabaseSeeder.php
+++ GMEDIA/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // Tambahkan data pengguna
         User::create([
-            'address' => '10.109.0.191',
+            'address' => '192.1168.1.15',
             'email' => 'admin@admin.com',
             'username' => 'admin',
             'password' => Hash::make('admin'),
