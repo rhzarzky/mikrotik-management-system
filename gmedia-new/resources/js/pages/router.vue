ce6f73b5dec49005eddc75ad8fb4418c4324edd2
wasiswsn <wasis1219@gmail.com>
2024-06-03 19:13:49 +0700

    add new vue

---
+++ gmedia-new/resources/js/pages/router.vue
@@ -0,0 +1,20 @@
+<script setup>
+import DemoSimpleTableBasics from '@/views/pages/tables/DemoSimpleTableBasics.vue'
+import DemoSimpleTableDensity from '@/views/pages/tables/DemoSimpleTableDensity.vue'
+import DemoSimpleTableFixedHeader from '@/views/pages/tables/DemoSimpleTableFixedHeader.vue'
+import DemoSimpleTableHeight from '@/views/pages/tables/DemoSimpleTableHeight.vue'
+import DemoSimpleTableTheme from '@/views/pages/tables/DemoSimpleTableTheme.vue'
+</script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <VCard title="Basic">
+        <VCardText>
+          You can fix the header of table by using the <code>fixed-header</code> prop.
+        </VCardText>
+        <DemoSimpleTableBasics />
+      </VCard>
+    </VCol>
+  </VRow>
+</template>
---
+++ gmedia-new/resources/js/pages/user.vue
@@ -0,0 +1,21 @@
+<script setup>
+import UserTable from '@/views/pages/tables/UserTable.vue'
+import UserActiveTable from '@/views/pages/tables/UserActiveTable.vue'
+</script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <VCard title="User Active">
+        <UserActiveTable />
+      </VCard>
+    </VCol>
+    
+    <VCol cols="12">
+      <VCard title="User List">
+        <UserTable />
+      </VCard>
+    </VCol>
+    
+  </VRow>
+</template>
