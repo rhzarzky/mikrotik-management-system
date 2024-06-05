92de9539998aa970aaba281a6ba31fa9d2f72167
Rheza Rizky <rhezarizky11@gmail.com>
2024-06-05 10:00:36 +0700

    cache

---
+++ storage/framework/views/89fa0bcae246872e8c8dc35af280ab2fd6f26881.php
@@ -0,0 +1,3 @@
+<?php echo e($uptime); ?>
+
+<?php /**PATH D:\File Rheza\gmedia-project\resources\views/realtime/uptime.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e93364dba2b7d96556c131ec0c7ecf60eaad70d7.php
@@ -0,0 +1,3 @@
+<div><?php echo e($cpu); ?> %</div>
+
+<?php /**PATH D:\File Rheza\gmedia-project\resources\views/realtime/status.blade.php ENDPATH**/ ?>
