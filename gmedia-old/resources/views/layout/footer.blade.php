420a12f1d016961f4756c8d09d5102383bf88420
Rheza Rizky <rhezarizky11@student.uns.ac.id>
2024-06-26 16:21:01 +0700

    footer

--- gmedia-old/resources/views/layout/footer.blade.php
+++ gmedia-old/resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
         <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Gmedia.Net</div>
+                        <div class="text-muted">Copyright &copy; Gmedia</div>
                         <div>
                             <a href="https://wa.me/6289512537245?text=Permisi%20Admin%20Ganteng,%20apakah%20bisa%20konsultasi%3F">Hubungi</a>
                             &middot;
